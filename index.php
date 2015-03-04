166d0cbbc3ee5488448b46da3828923efd13e3bf
Lluan <luanlove43@hotmail.com>
2015-03-04 12:23:41 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testando PHP</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<h2>Curso de PHP Básico</h2>
+
+	<a href="operadoresAritmeticos.php">Aritmetica</a>
+
+</body>
+</html>
